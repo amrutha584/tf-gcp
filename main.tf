de07dac95f7275c53f6cb35053a7a326149d2f1a
amrutha584 <39006496+amrutha584@users.noreply.github.com>
2026-03-10 15:12:56 +0530

    Update main.tf with the sql server

--- main.tf
+++ main.tf
@@ -6,3 +6,27 @@ resource "google_storage_bucket" "storage-bucket" {
   storage_class = var.storage_class
   force_destroy = var.force_destroy
 }
+
+resource "google_sql_database_instance" "sqlserver_trial" {
+  name             = var.sql_server_name
+  location = var.location
+  project= var.project_id
+  database_version = "SQLSERVER_2019_EXPRESS" # free-friendly edition
+
+  settings {
+    tier = "db-custom-1-3840" # small tier to save credits
+    backup_configuration {
+      enabled = true
+    }
+  }
+
+  root_password = "TrialPassword123!"
+  deletion_protection = false
+}
+
+resource "google_sql_user" "trial_user" {
+  name     = "testuser"
+  instance = google_sql_database_instance.sqlserver_trial.name
+  password = "SecureTrialPass!"
+}
+
